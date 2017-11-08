d11ac0d85609bc8f8c538a9c133a04a60133d432
Joyce Wong <wong.cy.joyce@gmail.com>
2017-11-08 18:05:14 +0000

    updated validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   CATEGORIES = ["Chinese", "Italian", "Japanese", "French", "Belgian"]
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :address, presence: true
   validates :category, inclusion: { in: CATEGORIES }
 
